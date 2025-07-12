3af835addaf5c7f3194c30e36d57457f06737627
reyanshtandi <mikutandi99@gmail.com>
2025-07-13 00:06:01 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: "build",
+    outDir: 'build',
   },
-  plugins: [tsconfigPaths(), react()],
 });
